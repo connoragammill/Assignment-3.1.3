0e0d0d1f945d6aa098f6c77144ded19b67bfb349
connor gammill <connor.gammill@outlook.com>
2024-11-18 14:59:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment_3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringInput = "This is a test string";
+            Console.WriteLine(stringInput);
+            char spaceChar = ' ';
+            int spaceCount = 0;
+            foreach(char c in stringInput)
+            {
+                if (c == spaceChar)
+                {
+                    spaceCount++;
+                }
+            }
+            Console.WriteLine(stringInput + " contains " + spaceCount + " spaces.");
+        }
+        
+    }
+}
